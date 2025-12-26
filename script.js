6aa505f66a92f50144976412c0715f145218b69b
100ratings <omentalistademian@gmail.com>
2025-12-25 22:48:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ function styleIndicatorOnce() {
     position: "fixed",
     right: "10px",
     left: "auto",
-    top: "72%",
+    top: "92%",
     transform: "translateY(-50%)",
     zIndex: "9999",
     background: "transparent",
@@ -394,4 +394,5 @@ deckEl.addEventListener("click", (e) => {
   if (awaitingRetry) return;
   if (e.target === cardImg) startDeck();
 });
+
 
